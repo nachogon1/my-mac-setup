29b71efb0c52b1e9f55e1449fd4bc8b07d1040fb
Ignacio Gonzalez Betegon <nachogon1@Ignacios-MacBook-Pro-2.local>
2025-04-20 01:18:17 +0200

    Add spark configs

---
+++ configs/spark/README.md
@@ -0,0 +1,6 @@
+## Requeriments
+python3.13
+java11
+pyspark==3.5.5
+notebook==7.4.0
+ipykernel==6.29.5
---
+++ configs/spark/spark-local.sh
@@ -0,0 +1,7 @@
+export SPARK_WORKER_INSTANCES=4
+SPARK_WORKER_CORES=2 
+SPARK_WORKER_MEMORY=2g
+SPARK_WORKER_INSTANCES=4 
+$SPARK_HOME/sbin/start-worker.sh spark://Ignacios-MacBook-Pro-2.local:7077
+$SPARK_HOME/sbin/start-master.sh
+pyspark
